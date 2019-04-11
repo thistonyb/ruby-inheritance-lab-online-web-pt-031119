bf387ea3b815d1f41f77bea3b1511192349c08f3
Tony Baker <this.tonybaker@gmail.com>
2019-04-11 19:01:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ require_relative "./user.rb"
 class Student < User
   attr_accessor
 
-  def initialize(first_name, last_name)
+  def initialize
     @first_name = first_name
     @last_name = last_name
     @knowledge = []
